8ac5c05655ef68b95383bac8d5d19643c2a3a8f0
KB <bhojakkeyur1@gmail.com>
2025-05-14 14:58:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,9 +44,6 @@ def predict():
 
     return render_template('index.html', prediction_text='Prediction: {}'.format(result))
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
 def ping_self():
     while True:
         try:
